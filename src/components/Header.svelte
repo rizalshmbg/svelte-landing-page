0ab75a54d7a33aa292dfba67f8addeb25b5044e9
rizalshmbg <sihombingrizal18@gmail.com>
2024-07-24 17:29:17 +0700

    completed functionality open modal for navbar

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { openModal } from '../store';
 </script>
 
 <header class="flex flex-col relative z-20">
@@ -10,7 +11,10 @@
 				Swoley <span class="text-indigo-400">Moley</span>
 			</h1>
 		</a>
-		<button class="md:hidden grid place-items-center">
+		<button
+			class="md:hidden grid place-items-center"
+			on:click={() => ($openModal = true)}
+		>
 			<i class="fa-solid fa-bars"></i>
 		</button>
 		<nav class="hidden md:flex items-center gap-4 lg:gap-6">
