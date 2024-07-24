fa1f9f824868a8a3d96405b8798278ac2e52a8e4
rizalshmbg <sihombingrizal18@gmail.com>
2024-07-24 15:03:16 +0700

    move the index mod class order-2 to medium screen only

--- src/components/ProductCard.svelte
+++ src/components/ProductCard.svelte
@@ -6,7 +6,10 @@
 <div
 	class="grid grid-cols-1 md:grid-cols-2 gap-14 md:gap-14 lg:gap-20 relative text-base"
 >
-	<div class="flex flex-col gap-8 md:gap-10">
+	<div
+		class={'flex flex-col gap-8 md:gap-10 pt-8 sm:pt-10 ' +
+			(index % 2 === 1 ? ' md:order-2' : ' ')}
+	>
 		<h4
 			class="text-2xl sm:text-3xl md:text-4xl max-w-[1000px] w-full font-medium relative pr-10 after:absolute after:top-full after:left-0 after:w-1/5 after:h-1.5 after:mt-1 after:bg-slate-900"
 		>
@@ -24,6 +27,23 @@
 					<p>{listItem}</p>
 				</div>
 			{/each}
+		</div>
+		<div class="flex items-center">
+			<button class="specialBtnDark mr-auto font-semibold">
+				<p>Try free today &rarr;</p>
+			</button>
+		</div>
+	</div>
+	<div class={'flex flex-col dropShadow overflow-hidden rounded-b-lg '}>
+		<div
+			class="rounded-t-xl h-8 sm:h-10 bg-white opacity-60 px-3 flex items-center gap-2"
+		>
+			{#each [1, 2, 3] as i}
+				<div class="rounded-full aspect-square w-2.5 sm:w-3 bg-indigo-300" />
+			{/each}
+		</div>
+		<div class="flex flex-col bg-white gap-4 flex-1">
+			<img src={productFeature.imgUrl} alt="product-img" />
 		</div>
 	</div>
 </div>
