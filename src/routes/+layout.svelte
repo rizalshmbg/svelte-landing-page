d9179b15856d572e0f421ecbec4d30ff030d014a
rizalshmbg <sihombingrizal18@gmail.com>
2024-07-24 16:59:40 +0700

    add footer component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <!-- A EQUIVALENT LEVEL ACTUALLY INHERITED FROM ANY PARENT ROUTES (WRAP ENTIRE APP) -->
 
 <script>
-  import "../app.css";
+	import '../app.css';
+	import Footer from './../components/Footer.svelte';
 </script>
 
 <slot />
+<Footer />
 
 <!-- header -->
 <!-- hero -->
