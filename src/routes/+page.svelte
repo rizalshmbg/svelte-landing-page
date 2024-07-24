35631bfe818e8e160f509d48ad59e2bcc9c4b35e
rizalshmbg <sihombingrizal18@gmail.com>
2024-07-24 14:11:27 +0700

    import product component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Hero from '../components/Hero.svelte';
+	import Product from '../components/Product.svelte';
 </script>
 
 <!-- RENDERED A PARTICULAR FOLDER ROUTE -->
 
 <main class="flex flex-col">
 	<Hero />
+	<Product />
 </main>
