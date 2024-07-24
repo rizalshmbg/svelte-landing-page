12f5a87524989cc86b5188172720eeb6addb124a
rizalshmbg <sihombingrizal18@gmail.com>
2024-07-24 16:58:58 +0700

    create a custom dark button

--- src/components/CTAs.svelte
+++ src/components/CTAs.svelte
@@ -1,8 +1,9 @@
 <script>
+	export let dark;
 </script>
 
 <div class="flex items-center gap-4">
-	<button class={'specialBtn'}>
+	<button class={'specialBtn' + (dark ? ' bg-[#181B34]' : ' ')}>
 		<p class="text-base sm:text-lg md:text-xl">Contact Us</p>
 	</button>
 	<button class={'specialBtnDark'}>
